45127663b7cbf368a25827c106f51e15e595734d
Ved Prakash <vedprakash@Veds-MacBook-Air.local>
2025-05-09 01:58:22 +0530

    landing_page

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import ClientLayout from '@/components/ClientLayout';
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ClientLayout>{children}</ClientLayout>;
-} 
